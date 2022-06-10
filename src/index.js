441c62574fbb0caa2a17dc5246a29e96d15b98e0
Jessica Wachtel <jlwachtel@gmail.com>
2022-06-10 09:25:04 -0400

    updating index and making sure git is connected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BasicApp from './BasicApp.js';
-// import App from './App.js';
+// import BasicApp from './BasicApp.js';
+import App from './App.js';
 
 ReactDOM.render(
-    <BasicApp/>,
-    // <App/>,
+    // <BasicApp/>,
+    <App/>,
     document.getElementById('root')
 );
